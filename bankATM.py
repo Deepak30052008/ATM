3c2f1ee94e335c4149ac747910a65b474e69d62c
Deepak <79149147+Deepak30052008@users.noreply.github.com>
2022-01-27 18:59:42 +0530

    Add files via upload

---
+++ bankATM.py
@@ -0,0 +1,16 @@
+class Automated_Teller_Machine :
+    def __init__(self,pinNumber,card):
+        self.pinNumber:input(pinNumber)
+        self.card:card
+    
+    def BalanceEnquiry(self,enquiry):
+        self.enquiry:enquiry
+        print(enquiry)
+
+    def CashWithdrawl(self,cash):
+        self.cash:cash
+
+ATM=Automated_Teller_Machine(4564654511,"cardEntered")
+ATM.BalanceEnquiry(800000)
+ATM.CashWithdrawl(input("Enter the amount you want to withdraw within the amount:"))
+print("withdrawal complete")
